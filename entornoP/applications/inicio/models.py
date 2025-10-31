74534115b47c35aa7dbdaa3b403e4b2c1d1ea35b
yannycontreras-sudo <yanny.contreras@alumnos.ucn.cl>
2025-10-31 19:22:42 -0300

    Aplicando .gitignore correctamente

---
+++ entornoP/applications/inicio/models.py
@@ -0,0 +1,123 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Estudiante(models.Model):
+    nombre = models.CharField('Nombre del estudiante',
+                              max_length=150, null=False)
+    apellido = models.CharField(
+        'Apellido del estudiante', max_length=150, null=False)
+    correo = models.CharField('Correo del estudiante',
+                              max_length=200, null=False)
+
+    def __str__(self):
+        return self.id+'-'+self.nombre
+
+
+class Evaluaciones(models.Model):
+    calificacion = models.FloatField(
+        'Calificación', max_length=100, null=False)
+    retroalimentacion = models.CharField('Retroalimentación', null=False)
+    fecha_evaluacion = models.DateField('Fecha de la evaluación')
+
+    def __str__(self):
+        return self
+
+
+class Reporte(models.Model):
+    nombre = models.CharField('Nombre del reporte', max_length=100, null=False)
+    fecha_generacion = models.DateField('Fecha de generación del reporte')
+    contenido_extraido = models.CharField('Contenido', null=False)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class Simulacion(models.Model):
+    fecha_inicio = models.DateField('Fecha de inicio de la simulación')
+    fecha_fin = models.DateField('Fecha de término de la simulación')
+    motivo_consulta = models.CharField(
+        'Motivo de la consulta', max_length=300, null=False)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class Administrador(models.Model):
+    nombre = models.CharField(
+        'Nombre del administrador', max_length=150, null=False)
+    apellido = models.CharField(
+        'Apellido del administrador', max_length=150, null=False)
+    correo = models.CharField(
+        'Correo del administrador', max_length=150, null=False)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class Diagnostico(models.Model):
+    hipotesis = models.CharField(
+        'Posible diagnostico o idea inicial sobre el caso', max_length=300, null=False)
+    justificacion = models.CharField(
+        'Justificación del diagnóstico', max_length=300, null=False)
+    fecha_envio = models.DateField('Fecha de envío del diagnóstico')
+    estado = models.CharField('Estado del diagnóstico',
+                              max_length=150, null=False)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class Examenfisico(models.Model):
+    fecha_registro = models.DateField('Fecha de registro')
+    fecha_examen = models.DateField('Fecha del examen físico')
+    hallazgos = models.CharField(
+        'Hallazgos en el examen físico', max_length=300, null=False)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class casoclinico(models.Model):
+    titulo = models.CharField(
+        'Nombre del caso clínico', max_length=300, null=False)
+    resumen = models.CharField(
+        'Resumen del caso clínico', max_length=300, null=False)
+    estado = models.CharField('Estado del caso', max_length=300, null=False)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class contenidomultimedia(models.Model):
+    tipo = models.CharField('Tipo de contenido', max_length=300, null=False)
+    url_contenido = models.URLField()
+    descripcion = models.CharField('Descripción', max_length=300, null=False)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class plantratamiento(models.Model):
+    objetivo = models.CharField(
+        'Objetivo del plan de tratamiento', max_length=300, null=False)
+    detalle = models.CharField('Detalle del plan', max_length=300, null=False)
+    fecha_envio = models.DateField('Fecha de envío del plan de tratamiento')
+    estado = models.CharField(
+        'Estado del plan de tratamiento', max_length=300, null=False)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class Paciente(models.Model):
+    nombre = models.CharField('Nombre del paciente',
+                              max_length=100, null=False)
+    apellido = models.CharField(
+        'Apellido del paciente', max_length=100, null=False)
+    correo = models.CharField('Correo del paciente',
+                              max_length=200, null=False)
+
+    def __str__(self):
+        return super().__str__()

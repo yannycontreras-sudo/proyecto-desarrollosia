347458993e2c3e78c916f3e341ae77b4f07fa068
yannycontreras-sudo <yanny.contreras@alumnos.ucn.cl>
2025-10-31 19:44:33 -0300

    Primera version app
    First

---
+++ rueba/settings/local.py
@@ -0,0 +1,25 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_kine',
+        'USER': 'user_proyecto',
+        'PASSWORD': 'kine123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'

c0c7c28084203663d3845d93ba02a72ceffc6732
yannycontreras-sudo <yanny.contreras@alumnos.ucn.cl>
2025-11-01 03:40:33 -0300

    Intento de la app con jazmin

---
+++ entornoP/applications/login/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Estudiante)
+admin.site.register(Evaluaciones)
+admin.site.register(Simulacion)
+admin.site.register(Caso_clinico)
+admin.site.register(Diagnostico)
+admin.site.register(Reporte)
+admin.site.register(Examen_fisico)
+admin.site.register(Contenido_multimedia)
+admin.site.register(Plan_de_tratamiento)
+admin.site.register(Paciente)

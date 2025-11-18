bd39174190efb62d92c7d60021d4dc4829e8f030
Millaray05 <millaray.rivera@alumnos.ucn.cl>
2025-11-17 21:40:31 -0300

    cargar template (modificar)

--- entornoP/applications/login/views.py
+++ entornoP/applications/login/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import (
+    TemplateView,
+
+
+)
+
+class Home(TemplateView):
+    template_name='login/home.html'

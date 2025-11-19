6f05d16c5148b8b9806c79eb4d424a4b7ff46cab
diegocastillo02-cell <diego.castillo02@alumnos.ucn.cl>
2025-11-19 08:27:24 -0300

    Cambios models.py login

--- entornoP/applications/login/admin.py
+++ entornoP/applications/login/admin.py
@@ -13,67 +13,6 @@ class EstudianteAdmin(admin.ModelAdmin):
     )
 
 
-class EvaluacionAdmin(admin.ModelAdmin):
-    list_display = (
-        'calificacion',
-        'retroalimentacion',
-        'fecha_evaluacion',
-
-
-    )
-
-
-class ReporteAdmin(admin.ModelAdmin):
-    list_display = (
-        'nombre',
-        'fecha_generacion',
-        'contenido_extraido',
-
-
-    )
-
-
-class SimulacionAdmin(admin.ModelAdmin):
-    list_display = (
-        'nombre',
-        'fecha_inicio',
-        'fecha_fin',
-        'motivo_consulta',
-        'nombre_estudiante',
-
-
-    )
-
-
-class DiagnosticoAdmin(admin.ModelAdmin):
-    list_display = (
-        'hipotesis',
-        'justificacion',
-        'fecha_envio',
-        'estado',
-
-
-    )
-
-
-class ExamenFisicoAdmin(admin.ModelAdmin):
-    list_display = (
-        'fecha_registro',
-        'fecha_examen',
-        'hallazgos',
-
-
-    )
-
-
-class CasoClinicoAdmin(admin.ModelAdmin):
-    list_display = (
-        'titulo',
-        'resumen',
-        'estado',
-
-
-    )
 
 
 class ContenidoMultimediaAdmin(admin.ModelAdmin):
@@ -107,17 +46,9 @@ class AdministradorAdmin(admin.ModelAdmin):
     )
 
 
-class PacienteAdmin(admin.ModelAdmin):
-    list_display = (
-        'nombre',
-        'apellido',
-        'correo',
 
-
-    )
 
 
 admin.site.register(Estudiante, EstudianteAdmin)
-admin.site.register(Evaluacion, EvaluacionAdmin)
 admin.site.register(ContenidoMultimedia, ContenidoMultimediaAdmin)
 

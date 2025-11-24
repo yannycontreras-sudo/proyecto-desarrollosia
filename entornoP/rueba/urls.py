519a094bada10412e75027537f10bd3aa0eae463
diegocastillo02-cell <diego.castillo02@alumnos.ucn.cl>
2025-11-24 12:59:21 -0300

    Inicio directo en login

--- entornoP/rueba/urls.py
+++ entornoP/rueba/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path,path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+
+    path("", RedirectView.as_view(pattern_name="gestiondeusuarios:login", permanent=False)),
+    
     path('admin/', admin.site.urls),
     path('', include('applications.core.urls')),
     path('accounts/', include('applications.gestiondeusuarios.urls')),

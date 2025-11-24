e140e968bb3f2474da9f16dc6b086418321fc392
diegocastillo02-cell <diego.castillo02@alumnos.ucn.cl>
2025-11-24 13:06:52 -0300

    Redirect directo al login si se quiere ir a home

--- entornoP/applications/core/views.py
+++ entornoP/applications/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.db import models
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
+
 
 # Create your views here.
 from django.views.generic import (

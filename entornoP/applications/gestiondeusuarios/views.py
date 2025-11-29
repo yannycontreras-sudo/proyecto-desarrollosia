82eccb87237d4a1aeaa208b679f71c3ed606ff91
Millaray05 <millaray.rivera@alumnos.ucn.cl>
2025-11-29 15:57:56 -0300

    redireccione automaticamente el inicio de sesion con mis cursospor, por parte del estudiante

--- entornoP/applications/gestiondeusuarios/views.py
+++ entornoP/applications/gestiondeusuarios/views.py
@@ -20,16 +20,31 @@ def registro_view(request):
 
 def login_view(request):
     if request.method == "POST":
-        form = LoginForm(request, data=request.POST)
+        form =LoginForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user()
+            user =form.get_user()
             login(request, user)
 
-            # si venías de una página protegida usa ?next=..., si no al home
-            next_url = request.GET.get("next") or "core:home"
-            return redirect(next_url)
+
+#si venia desde una pagina protegida, respeta ?next=
+            next_url = request.GET.get("next")
+            if next_url:
+                return redirect(next_url)
+# si no hay 'next=, redirige segun el rol
+            role= getattr(user, "role", None)
+
+            if role == "student":
+#aqui lo mandamos directo a sus cursos
+                return redirect("cursos:mis_cursos")
+            elif role == "teacher":
+#docente a la lista de cursos 
+                return redirect("cursos:lista")
+            elif role == "admin":
+#admin al home 
+                return redirect("core:home")
+            return redirect("core:home")
         else:
-            messages.error(request, "Usuario o contraseña incorrectos.")
+            messages.error(request, "Usuario o contraseña incorrecta.")
     else:
         form = LoginForm(request)
 

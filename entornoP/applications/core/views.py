0e9cb568f197d7b3d396dc431c14705b8ef4e3ea
Millaray05 <millaray.rivera@alumnos.ucn.cl>
2025-12-05 18:08:27 -0300

    Mejoramiento de Home de Docente

--- entornoP/applications/core/views.py
+++ entornoP/applications/core/views.py
@@ -1,16 +1,7 @@
-from django.shortcuts import render
+# core/views.py
 from django.db import models
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
-
-# Create your views here.
-from django.views.generic import (
-    TemplateView,
-
-
-)
 
 class TimeStampedModel(models.Model):
     """Modelo abstracto con timestamps estándar."""
@@ -19,6 +10,7 @@ class TimeStampedModel(models.Model):
 
     class Meta:
         abstract = True
+
 
 class HomeView(TemplateView):
     template_name = "home.html"
--- entornoP/rueba/urls.py
+++ entornoP/rueba/urls.py
@@ -1,36 +1,24 @@
+# rueba/urls.py
 """
 URL configuration for rueba project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path,path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
-
 
 urlpatterns = [
-
-    path("", RedirectView.as_view(pattern_name="gestiondeusuarios:login", permanent=False)),
-    
-
-    path("", RedirectView.as_view(pattern_name="gestiondeusuarios:login", permanent=False)),
-    
+    # Panel de administración
     path('admin/', admin.site.urls),
+
+    # HOME (usa applications.core.urls → HomeView → home.html)
     path('', include('applications.core.urls')),
+
+    # Usuarios (login, registro, logout, etc.)
     path('accounts/', include('applications.gestiondeusuarios.urls')),
+
+    # Cursos
     path("cursos/", include("applications.cursos.urls")),
 ]
 
